625afedb5f4bec98582d965ddc0b3bf903037e9d
Flo <flo69001@hotmail.fr>
2016-02-29 16:49:04 +0100

    first commit

---
+++ app/src/main/java/florian/michel/channelmessaging/Connection.java
@@ -0,0 +1,18 @@
+package florian.michel.channelmessaging;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Connection {
+
+
+}
---
+++ app/src/main/java/florian/michel/channelmessaging/LoginActivity.java
@@ -0,0 +1,88 @@
+package florian.michel.channelmessaging;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, OnWSUpdateListener {
+
+    private EditText login_id;
+    private EditText pass_id;
+    private Button validate_button;
+    private HashMap logs = new HashMap();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        validate_button = (Button) findViewById(R.id.validate_button);
+        login_id = (EditText) findViewById(R.id.login_id);
+        pass_id = (EditText) findViewById(R.id.pass_id);
+        validate_button.setOnClickListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        logs.put("username", login_id.getText().toString());
+        logs.put("password", pass_id.getText().toString());
+        WSRequestAsyncTask request = new WSRequestAsyncTask(this.getApplicationContext(),"http://www.raphaelbischof.fr/messaging/?function=connect", logs);
+        request.addWSRequestListener(this);
+        request.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onWSResponseUpdate(String response) {
+        Gson gson = new Gson();
+
+        String json = gson.toJson(response);
+        Log.d("reponse:",response);
+    }
+}
