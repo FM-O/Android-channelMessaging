215ed3c48edbe26c05ee33d18d3954bc67581c78
Flo <flo69001@hotmail.fr>
2016-03-01 17:13:41 +0100

    deleted some files

--- app/src/main/java/florian/michel/channelmessaging/OnWSUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package florian.michel.channelmessaging;
-
-/**
- * Created by Flo on 29/02/2016.
- */
-public interface OnWSUpdateListener {
-
-    public void onWSResponseUpdate(String response);
-}
